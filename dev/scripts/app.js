a6a0c47ae7737224113bfc83647515be8761df54
Lucas Knight <lucas.a.knight@outlook.com>
2018-05-18 12:28:42 -0400

    map rendering now

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Map from './components/map';
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      
+    }
+  }
     render() {
       return (
-        <div>
-          Hello
-        </div>
+        <Map/>
       )
     }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+// apikey: "fc7bda90e297088c088bd9cd062288e4",
